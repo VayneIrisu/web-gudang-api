e3b50ab88587d34f06013369caf1b27945b47824
FahmiCh <37687222+VayneIrisu@users.noreply.github.com>
2026-02-22 00:00:53 +0700

    Update config v2

--- config.php
+++ config.php
@@ -12,17 +12,14 @@ try {
         $pass,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-
-            // 🔥 WAJIB UNTUK RAILWAY
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]
     );
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode([
         "error" => "DB connection failed",
-        "detail" => $e->getMessage() // sementara
+        "detail" => $e->getMessage()
     ]);
     exit;
 }
