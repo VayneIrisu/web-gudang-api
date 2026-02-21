cd9ba481f1fd9dd8e3a2350cbb0e7f9d88b99c76
FahmiCh <37687222+VayneIrisu@users.noreply.github.com>
2026-02-21 23:55:01 +0700

    tes config untuk debug

--- config.php
+++ config.php
@@ -1,22 +1,12 @@
 <?php
-$host = getenv("MYSQLHOST");
-$db   = getenv("MYSQLDATABASE");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$port = getenv("MYSQLPORT");
+header("Content-Type: application/json");
 
-try {
-    $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(["error" => "DB connection failed"]);
-    exit;
-}
+echo json_encode([
+  "MYSQLHOST" => getenv("MYSQLHOST"),
+  "MYSQLPORT" => getenv("MYSQLPORT"),
+  "MYSQLDATABASE" => getenv("MYSQLDATABASE"),
+  "MYSQLUSER" => getenv("MYSQLUSER"),
+  "MYSQLPASSWORD_IS_SET" => getenv("MYSQLPASSWORD") ? "YES" : "NO",
+]);
+
+exit;
