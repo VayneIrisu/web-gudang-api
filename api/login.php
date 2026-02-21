ee82e11a44a8a837e93f5e315b8e7dbcd048a4ed
unknown <vayneirisu@gmail.com>
2026-02-22 00:32:20 +0700

    fix some bug and error

--- api/login.php
+++ api/login.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 /* === CORS HEADER === */
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://web-gudang-seven.vercel.app");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
