d98c069f4cc1b5ef0172b627f9d0b332be908cb0
unknown <vayneirisu@gmail.com>
2026-02-26 10:10:36 +0700

    feat: ganti cors ke domain baru

--- api/cors.php
+++ api/cors.php
@@ -3,7 +3,7 @@
 
 // ⚠️ PENTING: jangan ada spasi / BOM sebelum ini
 
-header("Access-Control-Allow-Origin: https://web-gudang-seven.vercel.app");
+header("Access-Control-Allow-Origin: https://web-gudang-stb.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
